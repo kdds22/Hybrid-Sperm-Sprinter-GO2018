027d722cc8b288ead67c6fbd3bedf38a39ebf5fd
Klaus Dellano <klausdell@hotmail.com>
2018-11-27 13:09:16 -0300

    Some changes in menu scene

--- scripts/Menu_Main.gd
+++ scripts/Menu_Main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var main = load("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Main.tscn")
 
 func _ready():
 	$Tween.interpolate_property($Sperm, "position", Vector2(-25, 166), Vector2(74, 84), 1.8, Tween.TRANS_BACK, Tween.EASE_IN)
@@ -7,6 +8,14 @@ func _ready():
 	$Tween.interpolate_property($figurantes_2, "position", Vector2(-100, 82), Vector2(0, 0), 2.2, Tween.TRANS_BACK, Tween.EASE_IN)
 	$Tween.interpolate_property($Logo, "position", Vector2(-95, 30), Vector2(100, 30), 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($Ovulo, "position", Vector2(300, -28), Vector2(229, 29), 1.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-	$Tween.interpolate_property($Start_btn, "rect_position", Vector2(270, 80), Vector2(150, 80), 2.8, Tween.TRANS_EXPO, Tween.EASE_IN)
+	$Tween.interpolate_property($Start_btn, "rect_position", Vector2(270, 80), Vector2(147, 80), 2.8, Tween.TRANS_EXPO, Tween.EASE_IN)
 	$Tween.interpolate_property($Exit_btn, "rect_position", Vector2(270, 110), Vector2(147, 110), 2.9, Tween.TRANS_EXPO, Tween.EASE_IN)
 	$Tween.start()
+
+
+func _on_Start_btn_pressed():
+	get_tree().change_scene_to(main)
+
+
+func _on_Exit_btn_pressed():
+	get_tree().quit()
