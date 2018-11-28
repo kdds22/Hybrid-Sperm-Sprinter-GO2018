81288c90eefd5db6271742193eaaa735815c7630
Klaus Dellano <klausdell@hotmail.com>
2018-11-28 18:43:52 -0300

    Added ovulo instance
    and added 0.1 to the timer node, to instantiate the walls

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,22 +2,43 @@ extends Node2D
 
 var pre_wall = preload("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Walls.tscn")
 var pre_coin = preload("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Coin.tscn")
-
+var pre_ovulo = preload("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Ovulo.tscn")
 
 func _ready():
 	randomize()
 	$Timer.start()
 	$UpTimer.start()
+	$Player.connect("dead", self, "game_over")
+
+
+func _process(delta):
+	pass
+
+
+func game_over():
+	get_tree().paused = true
+	$Walls.hide()
+	$GameOver.show()
+	
 
 
 func _on_Timer_timeout():
 	var wall = pre_wall.instance()
 	wall.position = $SpawnWalls/Position2D.position
-	add_child(wall)
+	$Walls.add_child(wall)
 
 
 func _on_UpTimer_timeout():
-	var coin = pre_coin.instance()
-	coin.position = $SpawnWalls/Position2D.position
-	add_child(coin)
+	var ovulo = pre_ovulo.instance()
+	var pos = int(rand_range(1, 4))
+	match pos:
+		1:
+			ovulo.position = $SpawnWalls/Position2D.position
+		2:
+			ovulo.position = $SpawnWalls/Position2D2.position
+		3:
+			ovulo.position = $SpawnWalls/Position2D3.position
+		4:
+			return
+	$PowerUps.add_child(ovulo)
 	$UpTimer.start()
