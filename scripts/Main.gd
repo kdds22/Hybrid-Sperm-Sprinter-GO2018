9d37e0d3cc9d69ed860c624f24ae68f62d77c366
Klaus Dellano <klausdell@hotmail.com>
2018-11-29 18:15:20 -0300

    Create Skin Shop

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,23 +3,59 @@ extends Node2D
 var pre_wall = preload("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Walls.tscn")
 var pre_coin = preload("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Coin.tscn")
 var pre_ovulo = preload("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Ovulo.tscn")
+var main_Menu = preload("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Menu_Main.tscn")
+
+var ovulos
+var current_score
+var highscore
+
+var started = false
+
+
+func update_current():
+	ovulos = GlobalSystem.ovulos
+	GlobalSystem.reset_current_score()
+	current_score = GlobalSystem.current_score
+	highscore = GlobalSystem.highscore
 
 func _ready():
+	update_current()
+	update_shop()
 	randomize()
+	started = true
 	$Timer.start()
 	$UpTimer.start()
 	$Player.connect("dead", self, "game_over")
+	$Walls.show()
+	$GameOver.hide()
+	$Shop.hide()
+	$PowerUps.show()
+	get_tree().paused = false
 
 
 func _process(delta):
-	pass
+	if started:
+		current_score += delta
 
 
 func game_over():
+	started = false
+	update_labels()
 	get_tree().paused = true
 	$Walls.hide()
 	$GameOver.show()
-	
+	$PowerUps.hide()
+
+
+func ovulo_count():
+	GlobalSystem.ovulos_count()
+	ovulos = GlobalSystem.ovulos
+
+func update_labels():
+	GlobalSystem.final_score(int(current_score))
+	$GameOver/Score_lbl.text = str(int(GlobalSystem.current_score))
+	$GameOver/Best_lbl.text = str(int(highscore))
+	$Shop/current_ovulo_lbl.text = str(int(GlobalSystem.ovulos))
 
 
 func _on_Timer_timeout():
@@ -42,3 +78,65 @@ func _on_UpTimer_timeout():
 			return
 	$PowerUps.add_child(ovulo)
 	$UpTimer.start()
+
+
+func _on_btn_Continue_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_btn_Back_pressed():
+	$Shop.hide()
+	$GameOver.show()
+
+
+func _on_btn_Shop_pressed():
+	$Shop.show()
+	$GameOver.hide()
+
+
+func _on_btn_BuyLong_pressed():
+	if GlobalSystem.ovulos >= 5:
+		GlobalSystem.change_type_player(2)
+		GlobalSystem.update_ovulos(-2)
+		update_shop()
+		
+
+
+func _on_btn_BuyHand_pressed():
+	if GlobalSystem.ovulos >= 10:
+		GlobalSystem.change_type_player(3)
+		GlobalSystem.update_ovulos(-10)
+		update_shop()
+
+
+func _on_btn_BuyCloak_pressed():
+	if GlobalSystem.ovulos >= 20:
+		GlobalSystem.change_type_player(4)
+		GlobalSystem.update_ovulos(-20)
+		update_shop()
+
+func update_shop():
+	match GlobalSystem.type_player:
+		1:
+			$Shop/Esperm_base.show()
+		2:
+			$Shop/Esperm_long.hide()
+			$Shop/Esperm_long_normal.show()
+			$Shop/long_lbl.hide()
+			$Shop/btn_BuyLong.hide()
+			$Shop/ovulo_long.hide()
+		3:
+			$Shop/Esperm_hand.hide()
+			$Shop/Esperm_hand_normal.show()
+			$Shop/hand_lbl.hide()
+			$Shop/btn_BuyHand.hide()
+			$Shop/ovulo_hand.hide()
+		4:
+			$Shop/Esperm_cloak.hide()
+			$Shop/Esperm_cloak_normal.show()
+			$Shop/cloak_lbl.hide()
+			$Shop/btn_BuyLong.hide()
+			$Shop/ovulo_cloak.hide()
+
+
+
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,44 +8,43 @@ var flag_tween = false
 signal dead
 
 
+func _ready():
+	choose_state()
+
 
 func _input(event):
-	if Input.is_key_pressed(KEY_SPACE) or Input.is_key_pressed(BUTTON_LEFT) and !event.is_echo():
+	if Input.is_key_pressed(KEY_SPACE) and !event.is_echo():
 		swin()
-#	if Input.is_key_pressed(KEY_H) and !event.is_echo(): #for testing hit
-#		hit()
-
 
 func swin():
 	linear_velocity = Vector2()
 	apply_impulse(Vector2(), Vector2(0, -speed))
-	choose_state()
+	_type()
+
 
 
 func choose_state():
+	type = GlobalSystem.type_player
+	_type()
+
+func _type():
 	match type:
 		1:
 			$AnimationPlayer.play("base_idle")
-			Engine.time_scale = 1
 		2:
 			$AnimationPlayer.play("long_idle")
-			Engine.time_scale = 1
 		3:
 			$AnimationPlayer.play("hand_idle")
-			Engine.time_scale = 1
 		4:
 			$AnimationPlayer.play("hero_idle")
-			Engine.time_scale = 1.2
 
 func hit():
-	type -= 1
-	choose_state()
-	if type <= 0:
-		death()
 	if !flag_tween:
 		flag_tween = true
 		$Tween.interpolate_property($Sprite, 'modulate', Color(1, 1, 1, 1), Color(1, 0, 0, 1), .2, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
 		$Tween.start()
+
+	death()
 	
 
 func _on_Tween_tween_completed(object, key):
@@ -56,15 +55,17 @@ func _on_Tween_tween_completed(object, key):
 
 
 func up():
-	if type >= 4:
-		return
-	else: 
-		type += 1
-		choose_state()
-	if !flag_tween:
-		flag_tween = true
-		$Tween.interpolate_property($Sprite, 'modulate', Color(1, 1, 1, 1), Color(0, 0, 1, 1), .2, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
-		$Tween.start()
+	get_parent().ovulo_count()
+	pass
+#	if type >= 4:
+#		return
+#	else: 
+#		type += 1
+#		choose_state()
+#	if !flag_tween:
+#		flag_tween = true
+#		$Tween.interpolate_property($Sprite, 'modulate', Color(1, 1, 1, 1), Color(0, 0, 1, 1), .2, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+#		$Tween.start()
 
 
 func death():
