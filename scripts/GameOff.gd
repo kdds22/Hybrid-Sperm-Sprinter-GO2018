4056a88d32a686d8cb6097f6e7d6115871df8070
Klaus Dellano <klausdell@hotmail.com>
2018-11-30 18:23:49 -0300

    Reorganized Files and fixed assets

--- scripts/GameOff.gd
+++ scripts/GameOff.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-onready var main = load("res://Hybrid-Sperm-Sprinter-GO2018/scenes/Menu_Main.tscn")
+onready var main = load("res://scenes/Menu_Main.tscn")
 
 
 func _on_Timer_timeout():
